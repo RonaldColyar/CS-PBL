70069eef666b5c73b78cee999234463e797182ef
RonaldColyar <phantexcreations@gmail.com>
2021-03-28 01:27:47 -0500

    init

---
+++ AudioPlayerWithWpf/WpfTest/FileSelector.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Win32;
+using System;
+
+namespace FileSelector
+{
+    public class FileHandler
+    {
+
+        private  OpenFileDialog openFileDialog = new OpenFileDialog();
+        public string filepath = string.Empty;
+        System.Media.SoundPlayer player;
+
+        private  void setUpFileDialog()
+        {
+            openFileDialog.InitialDirectory = "c:\\";
+           
+            openFileDialog.FilterIndex = 0;
+            //maybe better false..
+            openFileDialog.RestoreDirectory = true;
+        }
+        private void PlayAudio()
+        {
+
+        }
+        public  void SelectFileToPlay()
+        {
+            setUpFileDialog();
+            bool? result = openFileDialog.ShowDialog();
+            if (result == true)
+            {
+                filepath = openFileDialog.FileName;
+
+            }
+            else {
+                System.Windows.Forms.MessageBox.Show("Error Selecting Audio File!");
+            
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ AudioPlayerWithWpf/WpfTest/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using FileSelector;
+namespace WpfTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        FileHandler file_handler = new FileHandler();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void  Play_Audio(object sender, RoutedEventArgs e)
+        {
+            file_handler.SelectFileToPlay();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
